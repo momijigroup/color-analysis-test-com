e21256c77cfaa57271a415f8fc43b8d78918b46f
momijigroupinc-4506 <momijigroupinc@gmail.com>
2025-08-10 11:43:38 +0100

    Remove Google Tag Manager from all pages
    - Removed GTM script and noscript tags from layout.tsx
    - Removed Script import that was only used for GTM
    - Removed GTM verification code from metadata
    - Updated privacy policy pages to remove GTM mentions

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,3 @@
-import Script from 'next/script'
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
@@ -60,9 +59,7 @@ export const metadata: Metadata = {
       'max-snippet': -1,
     },
   },
-  verification: {
-    google: process.env.GOOGLE_VERIFICATION_CODE || 'GTM-5F888XQW',
-  },
+
   alternates: {
     canonical: "https://color-analysis.pro/",
   },
@@ -148,28 +145,9 @@ export default function RootLayout({
         <link rel="dns-prefetch" href="https://quiz.color-analysis.pro" />
       </head>
       <body className={inter.className}>
-        {/* Google Tag Manager (noscript) */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-5F888XQW"
-            height="0" 
-            width="0" 
-            style={{display: 'none', visibility: 'hidden'}}
-          />
-        </noscript>
-        {/* End Google Tag Manager (noscript) */}
+
         {children}
-        {/* Google Tag Manager */}
-        <Script id="google-tag-manager" strategy="afterInteractive">
-          {`
-            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
-            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
-            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
-            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
-            })(window,document,'script','dataLayer','GTM-5F888XQW');
-          `}
-        </Script>
-        {/* End Google Tag Manager */}
+
       </body>
     </html>
   );
